16a7485ef4edea24a7ed680a3a46812d9211a492
Caroline Tobin <ctobin25>
2021-01-11 11:28:19 -0500

    added function

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// Assignment Code
-
-console.log("JS loaded!");
-
+function passwordGenerator() {
+    var charLength = document.getElementById('maxLength').value;
+    if(charLength > 128 || charLength < 8){
+        alert('charlength does not meet requirement');
+        return;
+    }
+    var specialChars = document.getElementById('specialChars').checked;
+    var num = document.getElementById('num').checked;
+    var lowerCase = document.getElementById('lowerCase').checked;
+    var upperCase = document.getElementById('upperCase').checked;
+    if(specialChars === false && num === false && !lowerCase && !upperCase){
+        alert('Must pick one');
+        return;
+    }
